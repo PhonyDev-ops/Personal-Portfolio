f51845345c68db10cf449efc3d0b2813524f571a
Chan <espejo.christiandavid.c@gmail.com>
2026-09-04 18:12:41 +0800

    Added Database

---
+++ Components/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginAgain.Components.Data;
+using LoginAgain.Components.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAgain.Components.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
-using Blazorise;
-using Blazorise.Bootstrap5;
 using LoginAgain.Components;
+using Microsoft.EntityFrameworkCore;
+using LoginAgain.Components.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    ));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
